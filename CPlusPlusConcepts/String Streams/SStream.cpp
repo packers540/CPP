e728cfaaee23366e8dcc462c085f70017761f2fd
packers540 <nsypo001@odu.edu>
2016-03-20 14:57:55 -0400

    PolyMorphic Exceptions

--- CPlusPlusConcepts/String Streams/SStream.cpp
+++ CPlusPlusConcepts/String Streams/SStream.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <sstream>
+#include <exception>
+#include <typeinfo>
 
 using namespace std;
 
-struct DivideByZero {
-    double divisor;
-    DivideByZero(double x) : divisor(x) {}
-    const char* what () {
-        //Allows me to define custom exceptions while maintaining the convention and data type used in std exceptions
-        //I could also inherit the exception class and overwrite the virtual function what()
-        stringstream stm;
-        stm << "Can't divide " << divisor << " by 0." << endl;
-        string temp = stm.str();
-        return temp.c_str();
-    }
+class DivideByZero : public exception{
+    public:
+        double divisor;
+        DivideByZero(double x) : divisor(x) {}
+        virtual const char* what () const throw() {
+            //Allows me to define custom exceptions while maintaining the convention and data type used in std exceptions
+            //I could also inherit the exception class and overwrite the virtual function what()
+            stringstream stm;
+            stm << "Can't divide " << divisor << " by 0." << endl;
+            string temp = stm.str();
+            return temp.c_str();
+        }
 };
 
 double divide(double x, double y)
@@ -28,10 +31,15 @@ int main()
 {
     try {
        cout << divide(101.5, 12.2) << endl;
+       DivideByZero * test = 0;
+       typeid(*test);
        cout << divide(10.1, 0.0) << endl;
     }
 
-    catch (DivideByZero &e) {
+    //Since my DivideByZero exception class inherits from the standard exceptions class and overwrite the what() function
+    //The exceptions is thrown as reference so we can use polymorphism to look for both standard exceptions
+    //and our custom defined ones
+    catch (exception &e) {
         cerr << e.what();
         return 1;
     }
